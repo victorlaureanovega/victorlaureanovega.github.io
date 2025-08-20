9293db48551b45d50a09ce0e9a15eed7c96b6c2e
Victor <victorlaureanovega@gmail.com>
2025-08-19 19:48:24 -0600

    Blog

--- src/pages/Blog/PrologoTardio.jsx
+++ src/pages/Blog/PrologoTardio.jsx
@@ -4,7 +4,7 @@ function PrologoTardio() {
     return(
         <main className="entrada-main">
             <div className="regresar-blog">
-                <a href="/obice-y-antonomasia">Ir al blog</a>
+                <a href="obice-y-antonomasia">Ir al blog</a>
             </div>
             <div className="contenedor-texto">
                 <h1>Un prólogo tardío</h1>
